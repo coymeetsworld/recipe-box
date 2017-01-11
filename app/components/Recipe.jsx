f89ab9b94fe5580b494d31e6cbdcbf9af0e07cd5
Coy Sanders <coymeetsworld@gmail.com>
2017-01-11 03:36:18 -0800

    Added edit feature for recipes

--- app/components/Recipe.jsx
+++ app/components/Recipe.jsx
@@ -5,6 +5,7 @@ class Recipe extends React.Component {
 	constructor(props) {
 		super(props);
 		this.onDeleteRecipe = this.onDeleteRecipe.bind(this);
+		this.renderEditRecipe = this.renderEditRecipe.bind(this);
 	}
 
 	onDeleteRecipe(recipeId) {
@@ -13,29 +14,68 @@ class Recipe extends React.Component {
 		}
 	}
 	
-	render () {
+	renderEditRecipe(recipeId) {
+		return () => {
+			this.props.onEditRecipe(recipeId);
+		}
+	}
+	
+	handleUpdate(e) {
+		e.preventDefault();
+		let name = this.refs.updatedName.value;
+		this.refs.updatedName.value = '';
+		let ingredients = this.refs.updatedRecipeIngredients.value;
+		this.refs.updatedRecipeIngredients.value = '';
 		
+		let updatedRecipe = 
+		{
+			inEditMode: false,
+			name,
+			ingredients
+		};
+		
+		this.props.onUpdateRecipe(this.props.id, updatedRecipe);
+	}
+	
+	render () {
+		console.log("Render called");	
+		console.log(this.props);
 		var listIngredients = () => {
 			let id = 1;
 			return this.props.ingredients.split(',').map((ingredient) => {
 				return <li key={id++}>{ingredient}</li>	
 			});
 		}		
+		if (this.props.inEditMode) {
 
-		return (
-			<div className="recipe">
-				<div className="recipe-header">{this.props.name}</div>
-				<div className="recipe-body">
-					<ul>
-						{listIngredients()}
-					</ul>
+						/*<button onClick={this.renderUpdateRecipe(this.props.id)}>Update</button>*/
+			return (
+				<div className="recipe">
+					<form onSubmit={this.handleUpdate.bind(this)}>
+						<input type="text" ref="updatedName" defaultValue={this.props.name} />
+						<textarea ref="updatedRecipeIngredients" defaultValue={this.props.ingredients}/>
+						<button>Update</button>
+					</form>
 				</div>
-				<div className="recipe-controls">
-					<button>Edit</button>
-					<button onClick={this.onDeleteRecipe(this.props.id)}>Delete</button>	
-				</div>
-			</div>
-		)
+			);
+			
+		} else {
+			return (
+				<div className="recipe">
+					<div className="recipe-header">{this.props.name}</div>
+					<div className="recipe-body">
+						<ul>
+							{listIngredients()}
+						</ul>
+					</div>
+					<div className="recipe-controls">
+						<button onClick={this.renderEditRecipe(this.props.id)}>Edit</button>
+						<button onClick={this.onDeleteRecipe(this.props.id)}>Delete</button>	
+					</div>
+				</div>);
+		}
+		
+
 	}
 	
 }
--- app/components/RecipeBox.jsx
+++ app/components/RecipeBox.jsx
@@ -12,6 +12,8 @@ class RecipeBox extends React.Component {
 		};
 		this.handleAddRecipe = this.handleAddRecipe.bind(this);
 		this.handleDeleteRecipe = this.handleDeleteRecipe.bind(this);
+		this.handleEditRecipe = this.handleEditRecipe.bind(this);
+		this.handleUpdateRecipe = this.handleUpdateRecipe.bind(this);
 	}
 	
 	handleAddRecipe(recipe) {
@@ -25,15 +27,44 @@ class RecipeBox extends React.Component {
 			recipes : [...this.state.recipes.slice(0,recipeId), ...this.state.recipes.slice(recipeId+1)]
 		});
 	}
+	
+	handleEditRecipe(recipeId) {			
+		let recipe = this.state.recipes[recipeId];
+		recipe.inEditMode = true;
+		console.log("Handled");
+		let recipes = [...this.state.recipes.slice(0,recipeId), recipe, ...this.state.recipes.slice(recipeId+1)];
+		console.log(recipes);
+		
+		this.setState({
+			recipes
+		});
+	}
+
+	handleUpdateRecipe(recipeId, updatedRecipe) {
+		/* Good form to check if updatedRecipe had any changes? Or does React handle that for us? Is it more efficent if we take care of it before calling setState? */
+		console.log("Updating recipe: ");
+		console.log(updatedRecipe);
+		
+		let recipes = [...this.state.recipes.slice(0,recipeId), updatedRecipe, ...this.state.recipes.slice(recipeId+1)];
+		
+		this.setState({
+			recipes
+		});
+	}
 
 	render () {
 		var listRecipes = () => {
 			return this.state.recipes.map((recipe, recipeIndex) => {
+				console.log(recipe);
 				return (<Recipe key={recipeIndex}
 												id={recipeIndex}
 												name={recipe.name} 
 												ingredients={recipe.ingredients}
-												onDeleteRecipe={this.handleDeleteRecipe}/>)
+												onDeleteRecipe={this.handleDeleteRecipe}
+												onEditRecipe={this.handleEditRecipe}
+												onUpdateRecipe={this.handleUpdateRecipe}
+												inEditMode={recipe.inEditMode}/>)
+												
 			});
 		};
 		
